74aea9f419e5f41c657cd227a2287f8e4f8a3b19
Olivier <Olivier@PC-RYZEN>
2022-12-04 07:20:46 +0100

    Aoc 04-12

---
+++ Vendredi/src/aoc2022/day04/CampCleanup$1.java
@@ -0,0 +1,25 @@
+package aoc2022.day04;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CampCleanup$1 {
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+			int score = 0;
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] tokens = line.split(",");
+				String[] left = tokens[0].split("-");
+				int l1 = Integer.parseInt(left[0]);
+				int l2 = Integer.parseInt(left[1]);
+				String[] right = tokens[1].split("-");
+				int r1 = Integer.parseInt(right[0]);
+				int r2 = Integer.parseInt(right[1]);
+				if ((r1 <= l2 && l1 <= r2) || (r2 <= l1 && l2 <= r1)) score++;
+			}
+			System.out.println(score);
+		}
+	}
+}
