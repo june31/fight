ddfbbd32d0df5653cf728d49453fe5056d8b8dfb
Olivier <Olivier@PC-RYZEN>
2022-12-12 07:31:07 +0100

    aoc day 12

--- Vendredi/src/aoc2022/day14/Day14_1.java
+++ Vendredi/src/aoc2022/day14/Day14_1.java
@@ -1,10 +1,10 @@
-package aoc2022.day12;
+package aoc2022.day14;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Day12_1 {
+public class Day14_1 {
 	public static void main(String[] args) throws IOException {
 		try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
 			int score = 0;
