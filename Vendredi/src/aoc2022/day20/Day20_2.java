e3a1bfd378d9909254905001be5315e2400361ea
Olivier <Olivier@PC-RYZEN>
2022-12-20 08:32:25 +0100

    aoc day 20

--- Vendredi/src/aoc2022/day20/Day20_2.java
+++ Vendredi/src/aoc2022/day20/Day20_2.java
@@ -6,30 +6,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Day20_1 {
-	
+public class Day20_2 {
+
+	private static final long KEY = 811589153;
 	private static Pair[] t;
 	private static Pair[] t2;
 	private static int n;
-	
+
 	public static void main(String[] args) throws IOException {
 		List<Pair> list = new ArrayList<>();
 		try (BufferedReader reader = new BufferedReader(new FileReader("input2.txt"))) {
 			int index = 0;
 			String line;
 			while ((line = reader.readLine()) != null) {
-				list.add(new Pair(index++, Integer.parseInt(line)));
+				list.add(new Pair(index++, KEY * Long.parseLong(line)));
 			}
 		}
-		
+
 		t = list.toArray(new Pair[0]);
 		n = t.length;
 		t2 = new Pair[n - 1];
-		
-		for (int i = 0; i < n; i++) {
-			int id = seekIndex(i);
-			remove(id);
-			insert(t[id], c2(id + t[id].b));
+
+		for (int m = 0; m < 10; m++) {
+			for (int i = 0; i < n; i++) {
+				int id = seekIndex(i);
+				remove(id);
+				insert(t[id], c2(id + t[id].b));
+			}
 		}
 		int zId = seekZero();
 		System.out.println(t[c(zId + 1000)].b + t[c(zId + 2000)].b + t[c(zId + 3000)].b);
@@ -56,15 +59,19 @@ public class Day20_1 {
 		return -1;
 	}
 
-	private static int c(int i) {
-		return (i + 256 * n) % n;
+	private static int c(long i) {
+		int e = (int) (i % n);
+		if (e < 0) return e + n;
+		return e;
 	}	
 
-	private static int c2(int i) {
-		return (i + 256 * (n-1)) % (n-1);
+	private static int c2(long i) {
+		int e = (int) (i % (n-1));
+		if (e < 0) return e + n - 1;
+		return e;
 	}	
 
-	public static record Pair(int a, int b) {
+	public static record Pair(int a, long b) {
 		public String toString() {
 			return "" + b;
 		}
