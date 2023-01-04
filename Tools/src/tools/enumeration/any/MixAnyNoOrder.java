03aa9d2e0b4044dc5a24776f663740b04c7f0a97
Olivier <Olivier@PC-RYZEN>
2023-01-04 08:09:59 +0100

    Warning

--- Tools/src/tools/enumeration/any/MixAnyNoOrder.java
+++ Tools/src/tools/enumeration/any/MixAnyNoOrder.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import tools.enumeration.permutations.MixPermutations;
-
 public class MixAnyNoOrder<A> implements Iterable<List<A>> {
 
 	private final List<A> l;
