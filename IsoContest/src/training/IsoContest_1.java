4e89e85067f05d128955720fce4ab00c013782a5
Olivier <Olivier@PC-RYZEN>
2023-01-12 22:11:53 +0100

    IsoContest 1

--- IsoContest/src/training/IsoContest_1.java
+++ IsoContest/src/training/IsoContest_1.java
@@ -1,12 +1,62 @@
 package training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tools.scanner.Scan;
 
 public class IsoContest_1 {
 	
 	public static void main(String[] args) {
 		int n = Scan.readInt();
-		
-		System.out.println(n);
+		int v = Scan.readInt();
+		M[] ms = new M[v];
+		for (int i = 0; i < v; i++) ms[i] = new M(i);
+		for (int i = 0; i < n; i++) {
+			int a = Scan.readInt();
+			int b = Scan.readInt();
+			ms[a].children.add(ms[b]);
+			ms[b].fathers.add(ms[a]);
+		}
+		for (M m : ms) if (m.children.isEmpty()) m.leaf = 1;
+
+		int max = -1;
+		int sol = 0;
+		boolean found = true;
+		while (found) {
+			found = false;
+			for (int i = 0; i < v; i++) {
+				M m = ms[i];
+				if (m.children.size() - m.done == 0) {
+					for (M f : m.fathers) {
+						f.leaf += m.leaf;
+						f.done++;
+						m.done = 99999;
+						if (f.leaf > max) {
+							max = f.leaf;
+							sol = f.id;
+						}
+						found = true;
+					}
+				}
+			}
+		}
+		System.out.println(sol);
+	}
+	
+	static class M {
+		int c = 0;
+		int leaf = 0;
+		int done = 0;
+		List<M> fathers = new ArrayList<>();
+		List<M> children = new ArrayList<>();
+		private int id;
+		public M(int i) {
+			id = i;
+		}
+		@Override
+		public String toString() {
+			return id + " " + children.size() + "" + done + " - " + leaf;
+		}
 	}
 }
--- IsoContest/src/training/IsoContest_2.java
+++ IsoContest/src/training/IsoContest_2.java
@@ -1,12 +1,55 @@
 package training;
 
+import java.util.List;
+import java.util.function.BiConsumer;
+
 import tools.scanner.Scan;
 
 public class IsoContest_2 {
-	
+
+	static int max = -1;
+	static Cell best;
+
 	public static void main(String[] args) {
 		int n = Scan.readInt();
+		int v = Scan.readInt();
+		Cell[] ms = new Cell[v];
+		Tree<Cell> tree = new Tree<>();
+		for (int i = 0; i < v; i++) {
+			ms[i] = new Cell(i);
+			tree.add(ms[i]);
+		}
+		for (int i = 0; i < n; i++) tree.addChild(ms[Scan.readInt()], ms[Scan.readInt()]);
 		
-		System.out.println(n);
+		for (Cell c : tree.getLeafs()) c.score = 1;
+
+		tree.propagateLeafsToRoots((cell, father) -> {
+			father.score += cell.score;
+			if (father.score > max) {
+				max = father.score;
+				best = father;
+			}
+		});
+		System.out.println(best.id);
+	}
+	
+	static class Cell {
+		int score = 0;
+		private int id;
+		public Cell(int i) { id = i; }
+	}
+	
+	
+	
+	
+	
+	
+	
+	static class Tree<A> {
+		public void add(A cell) {}
+		public boolean isLeaf(A m) { return false; }
+		public void addChild(A cell1, A cell2) {}
+		public void propagateLeafsToRoots(BiConsumer<A, A> csm) {}
+		public List<A> getLeafs() { return null; }
 	}
 }
