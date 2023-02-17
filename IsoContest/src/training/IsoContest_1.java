982601c835b9724ec53890be077d0cc067a47c68
Olivier <Olivier@PC-RYZEN>
2023-02-17 08:56:11 +0100

    nothing

--- IsoContest/src/training/IsoContest_1.java
+++ IsoContest/src/training/IsoContest_1.java
@@ -1,16 +1,34 @@
 package training;
 
-import tools.output.Out;
+import java.util.List;
+
+import tools.enumeration.permutations.MixPermutations;
 import tools.scanner.Scan;
 
 public class IsoContest_1 {
 
 	public static void main(String[] args) {
-		int n = Scan.readInt();
-		String[] t = Scan.readStringArray(n);
-		for (int i = 0; i < n; i++) {
-			if (t[i].equals("" + new StringBuilder(t[i]).reverse())) Out.space(t[i]);
+		int a = Scan.readInt();
+		int b = Scan.readInt();
+		int c = Scan.readInt();
+		int d = Scan.readInt();
+		int e = Scan.readInt();
+		int f = Scan.readInt();
+		
+		var r = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		for (var l : new MixPermutations<>(r)) {
+			if (a == l.get(0) + l.get(1) + l.get(2)
+			&& b == l.get(3) + l.get(4) + l.get(5)
+			&& c == l.get(6) + l.get(7) + l.get(8)
+			&& d == l.get(0) + l.get(3) + l.get(6)
+			&& e == l.get(1) + l.get(4) + l.get(7)
+			&& f == l.get(2) + l.get(5) + l.get(8)) {
+ 				System.out.println("" + l.get(0) + l.get(1) + l.get(2) + "\n"
+						+ l.get(3) + l.get(4) + l.get(5) + "\n"
+						+ l.get(6) + l.get(7) + l.get(8));
+				break;
+			}
 		}
-		Out.flush();
 	}
 }
