934d68c46f24ece45ead7875a2810797471fe5ca
Olivier <Olivier@PC-RYZEN>
2023-12-06 06:23:35 +0100

    D06

--- Vendredi/src/aoc/Day_06_1.java
+++ Vendredi/src/aoc/Day_06_1.java
@@ -1,13 +1,21 @@
 package aoc;
 
-import tools.scanner.Scan;
+import tools.collections.int32.L;
+import tools.scanner.list.ScanL;
 
 public class Day_06_1 {
 	
 	public static void main(String[] args) {
-		String[] in = Scan.readRawStrings();
+		L times = ScanL.readLine();
+		L dists = ScanL.readLine();
 		
+		long mul = 1l;
+		for (int i = 0; i < times.size(); i++) {
+			int ok = 0;
+			for (int j = 1; j < times.get(i); j++) if (j*(times.get(i) - j) > dists.get(i)) ok++;
+			mul *= ok;
+		}
 		
-		System.out.println(0);
+		System.out.println(mul);
 	}
 }
--- Vendredi/src/aoc/Day_06_2.java
+++ Vendredi/src/aoc/Day_06_2.java
@@ -1,13 +1,17 @@
 package aoc;
 
+import tools.collections.int64.Ll;
 import tools.scanner.Scan;
 
 public class Day_06_2 {
 	
 	public static void main(String[] args) {
-		String[] in = Scan.readRawStrings();
-		
-		
-		System.out.println(0);
+		long t = new Ll(Scan.readLine().replace(" ", "")).get(0);
+		long d = new Ll(Scan.readLine().replace(" ", "")).get(0);
+
+		long ok = 0;
+		for (int j = 1; j < t; j++) if (j*(t - j) > d) ok++;
+
+		System.out.println(ok);
 	}
 }
