30b98a7aad82f92ff81602c772eced1140533d6a
Olivier Juin <olivier.juin@capgemini.com>
2023-12-12 15:00:03 +0100

    Compact D12

--- Vendredi/src/aoc/done/Day_12_2.java
+++ Vendredi/src/aoc/done/Day_12_2.java
@@ -8,13 +8,12 @@ import tools.scanner.Scan;
 
 public class Day_12_2 {
 
-	private static Map<String, Long> memo;
+	private static Map<String, Long> memo = new HashMap<>();
 
 	public static void main(String[] args) {
-		String[] in = Scan.readRawStrings();
-		memo = new HashMap<>();
+		String[] input = Scan.readRawStrings();
 		long s = 0;
-		for (var line: in) {
+		for (var line: input) {
 			String[] tk = line.split(" ");
 			L mvs = new L(tk[1]);
 			L vs = new L();
@@ -30,29 +29,20 @@ public class Day_12_2 {
 		if (rem != null) return rem;
 		long s = 0;
 		char c = t.charAt(0);
-		if (c == '.' || c == '?') s += val(t.substring(1), vs);
-		if (c == '#' || c == '?') {
-			boolean fine = !vs.isEmpty();
-			if (fine) {
-				int v = vs.get(0);
-				fine = t.length() >= v;
-				if (fine) {
-					for (int i = 0; i < v; i++) {
-						char k = t.charAt(i);
-						if (k != '#' && k != '?') {
-							fine = false;
-							break;
-						}
-					}
-				}
-				if (fine && t.length() > v) {
-					char k = t.charAt(v);
-					if (k != '.' && k != '?') fine = false;
-				}
-				if (fine) s += val(t.length() > v ? t.substring(v + 1) : "", vs.sub(1));
+		if (c == '.' || c == '?') s = val(t.substring(1), vs);
+		if ((c == '#' || c == '?') && !vs.isEmpty()) {
+			int v = vs.get(0);
+			boolean fine = t.length() >= v;
+			if (fine) for (int i = 0; i < v; i++) {
+				char k = t.charAt(i);
+				if (k != '#' && k != '?') fine = false;
 			}
+			if (fine && t.length() > v) {
+				char k = t.charAt(v);
+				if (k != '.' && k != '?') fine = false;
+			}
+			if (fine) s += val(t.length() > v ? t.substring(v + 1) : "", vs.sub(1));
 		}
-
 		memo.put(t + vs, s);
 		return s;
 	}
