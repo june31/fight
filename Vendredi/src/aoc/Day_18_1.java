9fbfe2bb51ffa53dda1611ba6ef875dc3aa147e3
Olivier <Olivier@PC-RYZEN>
2023-12-18 08:04:52 +0100

    D18 in progress

--- Vendredi/src/aoc/Day_18_1.java
+++ Vendredi/src/aoc/Day_18_1.java
@@ -1,15 +1,39 @@
 package aoc;
 
+import tools.bfs.BFS2D;
 import tools.scanner.Scan;
+import tools.tables.Table;
+import tools.tuple.Pos;
 
 public class Day_18_1 {
 	
+	private static BFS2D bfs;
 	public static void main(String[] args) {
 		String[] in = Scan.readRawStrings();
-		int[][] map = Scan.readMap0();
+		int[][] map = new int[1000][1000];
 		int L = map.length;
 		int C = map[0].length;
 		
-		System.out.println();
+		int l = 500;
+		int c = 500;
+		map[l][c] = '#';
+		
+		for (String a: in) {
+			String[] t = a.split(" ");
+			for (int i = 0; i < Integer.parseInt(t[1]); i++) {
+				if (t[0].equals("D")) l++;
+				if (t[0].equals("U")) l--;
+				if (t[0].equals("R")) c++;
+				if (t[0].equals("L")) c--;
+				map[l][c] = '#';
+			}
+		}
+
+		bfs = new BFS2D(map);
+		bfs.diffuse(new Pos(501, 501), '#', () -> { map[bfs.l2][bfs.c2] = '#'; return false; });
+		
+		int s = Table.findAll(map, '#').length;
+		
+		System.out.println(s);
 	}
 }
--- Vendredi/src/aoc/Day_18_3.java
+++ Vendredi/src/aoc/Day_18_3.java
@@ -5,14 +5,14 @@ import java.util.Collections;
 
 import tools.scanner.Scan;
 
-public class Day_18_2 {
-	
+public class Day_18_3 {
+
 	public static void main(String[] args) {
 		String[] in = Scan.readRawStrings();
-
+		
 		int minL = 0;
 		int maxL = 0;
-		
+
 		int l = 0;
 		int c = 0;
 		var vsList = new ArrayList<VS>();
@@ -20,32 +20,32 @@ public class Day_18_2 {
 
 		for (String a: in) {
 			String[] t = a.split(" ");
-			int x = Integer.parseInt(t[2].substring(2, 7), 16);
-			int d = t[2].charAt(7);
-			if (d == '0') c += x;
-			if (d == '1') { vsList.add(new VS(c, l, l + x - 1, false)); l += x; }
-			if (d == '2') { s += x; c -= x; }
-			if (d == '3') { vsList.add(new VS(c, l - x + 1, l, true)); l -= x; }
+			int x = Integer.parseInt(t[1]);
+			int d = t[0].charAt(0);
+			if (d == 'R') { s += 0; c += x; System.out.println(l + "R" + (x+1)); }
+			if (d == 'D') { vsList.add(new VS(c, l + 1, l + x - 1, false)); l += x; }
+			if (d == 'L') { s += x + 1; c -= x; System.out.println(l + "L" + (x+1)); }
+			if (d == 'U') { vsList.add(new VS(c, l - x + 1, l + 1, true)); l -= x; }
 			if (l < minL) minL = l;
 			if (l > maxL) maxL = l;
 		}
 
 		Collections.sort(vsList, (s1, s2) -> s1.c - s2.c);
-		
+
 		for (l = minL; l <= maxL; l++) {
 			for (VS vs: vsList) {
 				if (l >= vs.min && l <= vs.max) {
 					if (vs.up) c = vs.c;
 					else {
 						s += vs.c - c + 1;
-						//System.out.println(l + " " + (vs.c - c + 1));
+						System.out.println(l + " " + (vs.c - c + 1));
 					}
 				}
 			}
 		}
-		
+
 		System.out.println(s);
 	}
-	
+
 	private static record VS(int c, int min, int max, boolean up) {}
 }
