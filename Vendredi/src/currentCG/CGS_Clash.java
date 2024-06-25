9ac93063422ecb4dc34804d49f60bd1ecb334db2
Olivier Juin <olivier.juin@capgemini.com>
2024-06-25 23:31:50 +0200

    Isograd

--- Vendredi/src/currentCG/CGS_Clash.java
+++ Vendredi/src/currentCG/CGS_Clash.java
@@ -1,57 +1,76 @@
 package currentCG;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
-import tools.chrono.Chrono;
+import tools.bfs.BFSGraph;
 import tools.collections.int32.L;
-import tools.collections.int32.Si;
+import tools.collections.pos.Lp;
 import tools.scanner.Scan;
-import tools.scanner.list.ScanL;
-import tools.strings.S;
-import tools.tuple.II;
+import tools.structures.graph.node.Node;
+import tools.tuple.Pos;
 
 public class CGS_Clash {
 	public static void main(String[] args) {
-		Chrono.start();
-		Scan.readInt();
-		L l = new L();
-		for (int z = 0; z < Scan.readOnce(); z++) l.addAll(ScanL.readChars());
-		int n = Scan.readInt();
-		for (int i = 0; i < n; i++) {
-			used.clear();
-			Si s = new Si();
-			String[] ts = Scan.readString().split(",");
-			for (String t: ts) {
-				if (t.indexOf('-') == -1) s.add(t.charAt(0));
-				else for (int j = t.charAt(0); j <= t.charAt(2); j++) s.add(j);
+		int C = Scan.readInt();
+		int L = Scan.readInt();
+		int[][] map = new int[L][C];
+		int nb = Scan.readInt();
+		List<Lp> antas = new ArrayList<>();
+		for (int i = 1; i <= nb; i++) {
+			String s = Scan.readLine();
+			boolean zone = s.charAt(0) == 'D';
+			L a = new L(s);
+			if (zone) {
+				boolean found = false;
+				for (int c = a.get(0); c <= a.get(1); c++) {
+					for (int l = a.get(2); l <= a.get(3); l++) {
+						if (!found && map[l][c] != 0) {
+							Node.buildDual(i, map[l][c]);
+							found = true;
+						} else {
+							map[l][c] = i;
+						}
+					}
+				}
+			} else {
+				int p1 = map[a.get(2)][a.get(0)];
+				if (p1 != 0) {
+					Node.buildDual(i, p1);
+				} else {
+					map[a.get(2)][a.get(0)] = i;
+				}
+				int p2 = map[a.get(3)][a.get(1)];
+				if (p2 != 0) {
+					Node.buildDual(i, p2);
+				} else {
+					map[a.get(3)][a.get(1)] = i;
+				}
+				Lp anta = Lp.of(new Pos(a.get(2), a.get(0)), new Pos(a.get(3), a.get(1)));
+				antas.add(anta);
 			}
-			L cs = new L(s);
-			II res = find(l, cs, 0, l.size());
-			S.o(res.a(), res.b() - 1);
 		}
-		Chrono.stop();
-	}
-
-	private static Set<II> used = new HashSet<II>();
-	private static II find(L l, L cs, int p, int q) {
-		while (p < q && !cs.contains(l.get(p))) p++;
-		while (p < q && !cs.contains(l.get(q-1))) q--;
-		II ii = new II(p, q);
-		if (used.contains(ii)) return null;
-		used.add(ii);
 		
-		boolean[] tb = new boolean[cs.size()];
-		for (int i=p; i<q; i++) for (int j=0; j<cs.size(); j++) if (l.get(i) == cs.get(j)) tb[j] = true;
-		for (int i=0; i<cs.size(); i++) if (!tb[i]) return null;
-		II a1 = find(l, cs, p, q-1);
-		II a2 = find(l, cs, p+1, q);
-		if (a1 != null && a2 != null) {
-			if (a2.b() - a2.a() < a1.b() - a1.a()) return a2;
-			return a1;
+		int[] vals = new int[nb + 1];
+		vals[1] = 'A';
+		BFSGraph bfs = new BFSGraph();
+		bfs.diffuse(Node.get(1), () -> {
+			vals[bfs.n2.id] = 'A';
+			return false;
+		});
+		
+		for (Lp anta : antas) {
+			int p1 = map[anta.get(0).l][anta.get(0).c];
+			int p2 = map[anta.get(1).l][anta.get(1).c];
+			if (p1 == 0 || p2 == 0) {
+				System.out.println("IMPOSSIBLE");
+				return;
+			}
+			if (vals[p1] == vals[p2]) {
+				System.out.println("IMPOSSIBLE");
+				return;
+			}
 		}
-		if (a1 != null) return a1;
-		if (a2 != null) return a2;
-		return new II(p, q);
+
 	}
 }
