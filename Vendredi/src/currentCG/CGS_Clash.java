5d83eaca4fa425d1065978f586b3d9efe9af6e79
Olivier Juin <olivier.juin@capgemini.com>
2024-07-30 13:56:32 +0200

    In progress

--- Vendredi/src/currentCG/CGS_Clash.java
+++ Vendredi/src/currentCG/CGS_Clash.java
@@ -1,13 +1,32 @@
 package currentCG;
 
 import tools.scanner.Scan;
-import tools.strings.Regex;
 import tools.strings.S;
 
 public class CGS_Clash {
 	public static void main(String[] args) {
-		String t = Scan.readLine();
-		String r = Regex.escape(Scan.readLine().replace('?', '€')).replace("~", ".*").replace("€", ".");
-		S.o(t.matches(r) ? "MATCH" : "FAIL");
+		long l = Scan.readLong();
+		long h = Scan.readLong();
+		S.o(strobogrammatic(h) - strobogrammatic(l - 1));
+	}
+	
+	private static long strobogrammatic(long x) {
+		if (x == 0) return 1;
+		if (x < 0) return 0;
+		String s = "" + x;
+		int i = s.charAt(0) - '0';
+		int j = (int) x % 10;
+		long z = fullStrobogrammatic(s.length() - 1);
+		//z += 
+		if (i == 6 || i == 7) z *= 2;
+		
+		//long x 
+		return z;
+	}
+	
+	private static long fullStrobogrammatic(int l) {
+		if (l < 0) return 0;
+		if (l % 2 == 0) return (long) Math.pow(5, l/2);
+		else return 2 * (long) Math.pow(5, l/2);
 	}
 }
