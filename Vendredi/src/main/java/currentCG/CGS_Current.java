5de8cea7f3d3dabd0846cf1555cd346a815d20a0
Olivier Juin <olivier.juin@capgemini.com>
2024-10-14 14:46:24 +0200

    Glitch

--- Vendredi/src/main/java/currentCG/CGS_Current.java
+++ Vendredi/src/main/java/currentCG/CGS_Current.java
@@ -1,7 +1,6 @@
 package currentCG;
 
 import tools.F;
-import tools.math.Num;
 import tools.scanner.Scan;
 import tools.strings.S;
 import tools.tables.Table;
@@ -11,7 +10,7 @@ public class CGS_Current {
     public static void main(String[] args) throws Exception {
     	int w = Scan.readInt();
 		int h = Scan.readInt();
-		int[][] t = Table.wall(Scan.readIntArray(h, w), 0);
+		int[][] t = Table.shift0(Scan.readIntArray(h, w));
 		F.for1(h, w, (i, j) -> t[i][j] = Math.max(F.max0(i, k -> t[k][j] + t[i-k][j]).b(), F.max0(j, k -> t[i][k] + t[i][j-k]).b())); 
 		S.o(t[h][w]);
     }
