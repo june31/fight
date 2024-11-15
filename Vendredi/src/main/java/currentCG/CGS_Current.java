eb74c3b77394b7772a78cd0e4a7693177b56808e
Olivier Juin <olivier.juin@capgemini.com>
2024-11-15 15:10:21 +0100

    Start bulbs2

--- Vendredi/src/main/java/currentCG/CGS_Current.java
+++ Vendredi/src/main/java/currentCG/CGS_Current.java
@@ -1,74 +1,20 @@
 package currentCG;
 
-import tools.F;
-import tools.collections.int32.L;
-import tools.collections.multi.LAi;
-import tools.collections.string.Ls;
-import tools.mapper.MapLs;
-import tools.scanner.Scan;
-import tools.scanner.list.ScanLs;
+import tools.collections.bool.Lb;
 import tools.strings.S;
 
 public class CGS_Current {
-	private static int colors;
-	private static int columns;
-	private static int lines;
-	private static LAi lPositions;
-	private static L lCorrect;
-	private static L lMisplaced;
 
 	public static void main(String[] args) throws Exception {
-		colors = Scan.readInt();
-		columns = Scan.readInt();
-		lines = Scan.readInt();
-		Ls rawText = ScanLs.readLines(lines);
-		lPositions = LAi.fromLsDigits(rawText.column(0));
-		lCorrect = MapLs.toL(rawText.column(1));
-		lMisplaced = MapLs.toL(rawText.column(2));
-		recurseOnExact(new L(), lCorrect);
+		for (int i = 0; i < 256; i++) S.o(i >> 4, i & 15, recurse(new Lb(i >> 4, 4), new Lb(i & 15, 4))); 
 	}
-
-	private static void recurseOnExact(L history, L lCorrect) {
-		int depth = history.size();
-		if (depth == columns) {
-			if (validate(history)) S.o(history.join(""));
-			return;
-		}
-		COLOR: for (int color = 0; color < colors; color++) {
-			L lNewCorrect = lCorrect.copy();
-			for (int line = 0; line < lines; line++) {
-				int c = lPositions.get(line)[depth];
-				if (c == color) {
-					int correct = lCorrect.get(line);
-					if (correct == 0) continue COLOR;
-					lNewCorrect.set(line, correct - 1);
-				}
-				else if (lCorrect.get(line) >= columns - depth) continue COLOR;
-			}
-			L newHistory = history.copy();
-			newHistory.add(color);
-			recurseOnExact(newHistory, lNewCorrect);
-		}
-	}
-
-	private static boolean validate(L history) {
-		for (int line = 0; line < lines; line++) {
-			L solution = new L(history);
-			int[] pos = lPositions.get(line).clone();
-			for (int i = 0; i < columns; i++)
-				if (pos[i] == solution.get(i)) {
-					pos[i] = colors;
-					solution.set(i, colors + 1);
-				}
-			int[] stats = F.stats0(colors + 2, pos);
-			int misplaced = lMisplaced.get(line);
-			for (int s: solution)
-				if (stats[s] > 0) {
-					misplaced--;
-					stats[s]--;
-				}
-			if (misplaced != 0) return false;
-		}
-		return true;
+	
+	private static int recurse(Lb start, Lb end) {
+		if (start.isEmpty()) return 0;
+		if (start.first() == end.first()) return recurse(start.subbedStart(1), end.subbedStart(1));
+		int s = start.size();
+		if (s == 1) return 1;
+		Lb mid = new Lb(1 << (s - 2), s - 1);
+		return 1 + recurse(start.subbedStart(1), mid) + recurse(mid, end.subbedStart(1));
 	}
 }
