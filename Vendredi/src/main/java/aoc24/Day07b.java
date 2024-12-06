25ae6dce2663baa57f3c0a41e2f0949116b69074
Olivier <Olivier@PC-RYZEN>
2024-12-06 06:58:42 +0100

    Prepare day 7

--- Vendredi/src/main/java/aoc24/Day07b.java
+++ Vendredi/src/main/java/aoc24/Day07b.java
@@ -3,7 +3,7 @@ package aoc24;
 import tools.scanner.list.ScanLs;
 import tools.strings.S;
 
-public class Day06b {
+public class Day07b {
 	public static void main(String[] args) {
 		var l = ScanLs.readRaw();
 		
