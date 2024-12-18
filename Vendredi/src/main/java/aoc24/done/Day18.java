5dd4f51f73d65ff6042e79bfb2d0bba85b735e98
Olivier Juin <olivier.juin@capgemini.com>
2024-12-18 12:09:24 +0100

    Tiny 18

--- Vendredi/src/main/java/aoc24/done/Day18.java
+++ Vendredi/src/main/java/aoc24/done/Day18.java
@@ -1,23 +1,19 @@
 package aoc24.done;
 
 import tools.bfs.BFS;
+import tools.collections.pos.Lp;
 import tools.scanner.list.ScanLp;
 import tools.strings.S;
-import tools.tuple.Pos;
 
 public class Day18 {
 	public static void main(String[] args) {
-		var l = ScanLp.readRawCL();
+		Lp l = ScanLp.readRawCL();
 		var map = new int[71][71];
-		for (int i = 0; i < 1024; i++) map[l.get(i).l][l.get(i).c] = '#';
 		BFS b = new BFS(map).setWall('#').setEnd(70, 70);
-		S.o(b.diffuse(0, 0).turn);
-		Pos p = null;
-		for (int i = 1024; b.found; i++) {
-			p = l.get(i);
-			map[p.l][p.c] = '#';
-			b.diffuse(0, 0);
+		for (int i = 0; i == 0 || b.found; i++) {
+			if (i == 1024) S.o(b.diffuse(0, 0).turn);
+			map[l.get(i).l][l.get(i).c] = '#';
+			if (!b.diffuse(0, 0).found) S.o(l.get(i).c + "," + l.get(i).l);
 		}
-		S.o(p.c + "," + p.l);
 	}
 }
