db13b2c6b71b00d190a688d724340ef38b4926a8
Olivier Juin <olivier.juin@capgemini.com>
2024-12-18 11:51:47 +0100

    Day 18 reworked

--- Vendredi/src/main/java/aoc24/done/Day18.java
+++ Vendredi/src/main/java/aoc24/done/Day18.java
@@ -9,13 +9,13 @@ public class Day18 {
 	public static void main(String[] args) {
 		var l = ScanLp.readRawCL();
 		var map = new int[71][71];
-		BFS b = new BFS(map).setWall('#').setEnd(70, 70);
 		for (int i = 0; i < 1024; i++) map[l.get(i).l][l.get(i).c] = '#';
+		BFS b = new BFS(map).setWall('#').setEnd(70, 70);
 		S.o(b.diffuse(0, 0).turn);
 		Pos p = null;
 		for (int i = 1024; b.found; i++) {
 			p = l.get(i);
-			map[l.get(i).l][p.c] = '#';
+			map[p.l][p.c] = '#';
 			b.diffuse(0, 0);
 		}
 		S.o(p.c + "," + p.l);
