94144e5daddf77a504cf538ea0514a953d4f58af
Olivier <Olivier@PC-RYZEN>
2024-12-22 19:03:41 +0100

    Day 22

--- Vendredi/src/main/java/aoc24/Day23a.java
+++ Vendredi/src/main/java/aoc24/Day23a.java
@@ -3,7 +3,7 @@ package aoc24;
 import tools.scanner.list.ScanLs;
 import tools.strings.S;
 
-public class Day22a {
+public class Day23a {
 	private static long z = 0;
 	public static void main(String[] args) {
 		var ls = ScanLs.readRaw();
---
+++ Vendredi/src/main/java/aoc24/done/Day22a.java
@@ -0,0 +1,23 @@
+package aoc24.done;
+
+import tools.scanner.list.ScanLl;
+import tools.strings.S;
+
+public class Day22a {
+	private static long z = 0;
+	public static void main(String[] args) {
+		var l = ScanLl.readRaw();
+		for (var x: l) {
+			for (int i = 0; i < 2000; i++) {
+				x ^= x << 6;
+				x &= 0xffffff;
+				x ^= x >> 5;
+				x &= 0xffffff;
+				x ^= x << 11;
+				x &= 0xffffff;
+			}
+			z += x;
+		}
+		S.o(z);
+	}
+}
